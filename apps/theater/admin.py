cdbc7e308371f5a12f64e2e1f1462c9154391526
sachint97 <cssachinshoba@gmail.com>
2023-05-09 23:56:33 +0530

    model_updated

--- apps/theater/admin.py
+++ apps/theater/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(City)
+admin.site.register(Movie)
+admin.site.register(Media)
+admin.site.register(Theater)
+admin.site.register(Screen)
+admin.site.register(Seat)
+admin.site.register(Show)
+admin.site.register(Booking)
+admin.site.register(SelectedSeat)
+
